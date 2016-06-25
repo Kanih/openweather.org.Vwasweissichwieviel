bad2010010101872723b896bad87febd17075cc0
Kanih <nicolaikaschta@Nicolais-iMac-2.fritz.box>
2016-06-25 15:24:05 +0200

    View1 fertig

--- openweather.org.Vwasweissichwieviel/ViewController.swift
+++ openweather.org.Vwasweissichwieviel/ViewController.swift
@@ -34,9 +34,12 @@ class ViewController: UIViewController {
     @IBOutlet weak var tempmax: UILabel!
     @IBOutlet weak var windspd: UILabel!
     @IBOutlet weak var wdir: UILabel!
+    @IBOutlet weak var press: UILabel!
+    @IBOutlet weak var humidity: UILabel!
+    @IBOutlet weak var clouds: UILabel!
     
     func connect(){
-        let url: String = "http://api.openweathermap.org/data/2.5/weather?q=Hockenheim,de&appid=8fac47973bfc7e3f73b113e9b0171517"
+        let url: String = "http://api.openweathermap.org/data/2.5/weather?q=Hockenheim,de&appid=8fac47973bfc7e3f73b113e9b0171517&lang=de"
         let nsUrl: NSURL = NSURL(string: url)!
         let request: NSURLRequest = NSURLRequest(URL: nsUrl)
         let nsURLConnection: NSURLConnection = NSURLConnection(request: request, delegate: self, startImmediately: false)!
@@ -65,7 +68,7 @@ class ViewController: UIViewController {
             let tempMaxCelsius = tempMaxKelvin - 273.15
             tempmax.text = (NSString(format: "%.2f", tempMaxCelsius) as String)+" °C" as String
             let druck = main["pressure"] as! Double
-            ort.text = (NSString(format: "%.0f", druck) as String)+" hPa" as String
+            press.text = (NSString(format: "%.0f", druck) as String)+" hPa" as String
             let weather: NSArray = (jsonData["weather"] as? NSArray)!
             let weatherData: NSDictionary = weather[0] as! NSDictionary
             let description = String(weatherData["description"] as! NSString)
@@ -75,6 +78,14 @@ class ViewController: UIViewController {
             windspd.text = (NSString(format: "%.1f", wspeed) as String)+" m/s" as String
             let windir = wind["deg"] as! Double
             wdir.text = (NSString(format: "%.1f", windir) as String)+"°" as String
+            let name: NSString = jsonData["name"] as! NSString
+            ort.text = name as String
+            let humid = main["humidity"] as! Double
+            humidity.text = (NSString(format: "%.0f", humid) as String)+"%" as String
+            let cloud: NSDictionary = jsonData["clouds"] as! NSDictionary
+            let overcast = cloud["all"] as! Double
+            clouds.text = "Überdeckung "+(NSString(format: "%.0f", overcast) as String)+"%" as String
+
             
             //let icon = String(weatherData["icon"] as! NSString)
             //let image1 = UIImage(named: icon)
